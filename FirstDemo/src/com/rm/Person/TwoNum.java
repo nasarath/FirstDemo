baff1edbc94ef48550b7b828fce207c164bb81de
nasarath <nasarath@nasarath-PC>
2020-01-29 23:53:05 +0530

    add Welcome page

---
+++ FirstDemo/src/com/rm/Person/TwoNum.java
@@ -0,0 +1,11 @@
+package com.rm.Person;
+
+public class TwoNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to Room");
+	}
+
+}
